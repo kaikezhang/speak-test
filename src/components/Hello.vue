42e0fd7a7939e7ef0ef1895f357d2dc10f1ddbf6
Kaike Zhang <kaikezhang@gmail.com>
2017-09-22 10:31:28 -0400

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <div>
+    <span>{{text}}</span>
+    <q-btn icon="create" @click="sayHi">Click to speak</q-btn>
+  </div>
+</template>
+
+<script>
+import { QBtn } from 'quasar'
+
+export default {
+  data() {
+    text: ''
+  },
+  components: {
+    QBtn
+  },
+  methods: {
+    sayHi() {
+      console.log("You clicked me.")
+    	responsiveVoice.speak('hi there!')
+      this.text = 'Hi there!'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
